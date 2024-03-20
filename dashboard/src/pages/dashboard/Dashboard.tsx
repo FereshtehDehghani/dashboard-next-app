e70800b6f219c3867428d3d4e4712b0732be715c
FereshtehDehghani <fereshtehdehghani963@gmail.com>
2024-03-20 13:22:09 +0330

    design dashboard

--- dashboard/src/pages/dashboard/Dashboard.tsx
+++ dashboard/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,25 @@
+import { Grid, Paper } from "@mui/material";
 import React from "react";
+import scss from "./dashboard.module.scss";
 
 const Dashboard = () => {
-	return <div>dashborad</div>;
+	return (
+		<>
+			<Grid container spacing={2} marginY={3} justify="center">
+				<Grid item md={4} xs={12}>
+					<Paper>md=4</Paper>
+				</Grid>
+				<Grid item md={4} xs={12}>
+					<Paper>md=4</Paper>
+				</Grid>
+				<Grid item md={4} xs={12}>
+					<Paper>md=4</Paper>
+				</Grid>
+			</Grid>
+			<Grid container xs={12} marginY={5}>
+				<Paper>xs=8</Paper>
+			</Grid>
+		</>
+	);
 };
-
 export default Dashboard;
